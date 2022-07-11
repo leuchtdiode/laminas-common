23b1205ee1f82c2035c324474768c6d9e10f8f2e
Alex Schloegl <alexander.schloegl@compass.at>
2022-07-11 15:53:41 +0200

    Distance was miles, is not kilometers

--- src/Db/Functions/Distance.php
+++ src/Db/Functions/Distance.php
@@ -10,43 +10,49 @@ class Distance extends FunctionNode
 {
 	const NAME = 'DISTANCE';
 
-	private $sourceLatitude       = null;
-	private $sourceLongitude      = null;
-	private $destinationLatitude  = null;
-	private $destinationLongitude = null;
+	const EARTH_DIAMETER = 12742; // 2 * Earth's radius (6371 km)
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getSql(SqlWalker $walker): string
-	{
-		$formulae = '(ACOS(SIN(%s * PI() / 180) * SIN(%s * PI() / 180) + COS(%s * PI() / 180) * COS(%s * PI() / 180) * COS((%s - %s) * PI() / 180)) * 180 / PI()) * 60 * 1.1515';
+	protected $latOrigin;
+	protected $lngOrigin;
+	protected $latDestination;
+	protected $lngDestination;
 
-		return sprintf(
-			$formulae,
-			$this->destinationLatitude->dispatch($walker),
-			$this->sourceLatitude->dispatch($walker),
-			$this->destinationLatitude->dispatch($walker),
-			$this->sourceLatitude->dispatch($walker),
-			$this->destinationLongitude->dispatch($walker),
-			$this->sourceLongitude->dispatch($walker)
-		);
-	}
-
-	/**
-	 * {@inheritdoc}
-	 */
 	public function parse(Parser $parser): void
 	{
 		$parser->match(Lexer::T_IDENTIFIER);
 		$parser->match(Lexer::T_OPEN_PARENTHESIS);
-		$this->sourceLatitude = $parser->SimpleArithmeticExpression();
+		$this->latOrigin = $parser->ArithmeticExpression();
 		$parser->match(Lexer::T_COMMA);
-		$this->sourceLongitude = $parser->SimpleArithmeticExpression();
+		$this->lngOrigin = $parser->ArithmeticExpression();
 		$parser->match(Lexer::T_COMMA);
-		$this->destinationLatitude = $parser->SimpleArithmeticExpression();
+		$this->latDestination = $parser->ArithmeticExpression();
 		$parser->match(Lexer::T_COMMA);
-		$this->destinationLongitude = $parser->SimpleArithmeticExpression();
+		$this->lngDestination = $parser->ArithmeticExpression();
 		$parser->match(Lexer::T_CLOSE_PARENTHESIS);
+	}
+
+	public function getSql(SqlWalker $sqlWalker): string
+	{
+		/*
+		 * Giving each argument only once and using %1$s, %2$s, ... doesn't work. Would result in:
+		 * SQLSTATE[HY093]: Invalid parameter number: number of bound variables does not match number of tokens
+		 */
+		// formula adapted from http://www.scribd.com/doc/2569355/Geo-Distance-Search-with-MySQL
+		// originally returns distance in miles: 3956 * 2 * ASIN(SQRT(POWER(SIN((orig.lat - dest.lat) * PI()/180 / 2), 2) + COS(orig.lat * PI()/180) * COS(dest.lat * PI()/180) * POWER(SIN((orig.lon - dest.lon) * PI()/180 / 2), 2)))
+		return sprintf(
+			$this->getSqlWithPlaceholders(),
+			self::EARTH_DIAMETER,
+			$sqlWalker->walkArithmeticPrimary($this->latOrigin),
+			$sqlWalker->walkArithmeticPrimary($this->latDestination),
+			$sqlWalker->walkArithmeticPrimary($this->latOrigin),
+			$sqlWalker->walkArithmeticPrimary($this->latDestination),
+			$sqlWalker->walkArithmeticPrimary($this->lngOrigin),
+			$sqlWalker->walkArithmeticPrimary($this->lngDestination)
+		);
+	}
+
+	protected function getSqlWithPlaceholders()
+	{
+		return '%s * ASIN(SQRT(POWER(SIN((%s - %s) * PI()/360), 2) + COS(%s * PI()/180) * COS(%s * PI()/180) * POWER(SIN((%s - %s) * PI()/360), 2)))';
 	}
 }
